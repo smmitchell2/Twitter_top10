2fd0e6b927bbaab91b831865a8ef86ef1ec5583d
Shawn Mitchell <smmitchell3@gmail.com>
2018-02-08 08:55:16 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://apps.twitter.com/app/new
 #--------------------------------------------------------------------------------
 
-consumer_key = "oJ0ZrqcAW7NMktTABiswoMQ6V"
-consumer_secret = "enivHAHeOuBhRJmg3iQiHYSygFrUWrMVeBT6WZ7MuD6b7ydRvf"
-access_key = "958897003441860610-DO04OU3NJQ3334mTyv0YEZSkKed2zCN"
-access_secret = "AS2lqEhCl5uLGWjbDfmnyeZIYtXPvgM9llWQ71sDNZwHm"
+consumer_key = "consumer key"
+consumer_secret = "secret key"
+access_key = "access key"
+access_secret = "secret key"
